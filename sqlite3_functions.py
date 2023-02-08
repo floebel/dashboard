36c105ef4e74397d02c0012d5add936d86d6b106
Fulton Loebel <fulton.loebel@gmail.com>
2023-02-08 17:16:17 -0600

    commit

--- sqlite3_functions.py
+++ sqlite3_functions.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 
 import sqlite3
-import sqlalchemy
+#import sqlalchemy
 #from sqlalchemy.ext.automap import automap_base
 #from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func, inspect 
+#from sqlalchemy import create_engine, func, inspect 
 #Create engine 
 from math import sqrt
 import math
